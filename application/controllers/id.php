6c2e7e8320562a65ecf7918ef5928f6fbdb366e6
z1FFy <ziffyweb@gmail.com>
2013-11-15 15:51:19 +0400

    ID secure and modifled

--- application/controllers/id.php
+++ application/controllers/id.php
@@ -6,24 +6,27 @@ class Id extends CI_Controller {
     		$this->load->model('db_module');
 	}
 
-
-
-	function index() {
-
- 	$user_id=$this->session->userdata('user_id');
-	$logged = $this->session->userdata('logged_in');
-
-	$url_id = $this->uri->segment(1);
+private function _get_url_id()
+{
+  	$url_id = $this->uri->segment(1);
 	$url_id = trim($url_id, " \id.");
-
-	$podtvr=0;
-
-	$data_user = $this->db_module->get_user_by_id($url_id);
-
-	$whopage='none';
+	return $url_id;
+}
+private function _get_whopage($url_id,$user_id)
+{
+$whopage='none';
 	if ($user_id == $url_id) {
 		$whopage='my';
 	}
+}
+	function index() {
+	$url_id= $this->_get_url_id();
+ 	$user_id=$this->session->userdata('user_id');
+	$logged = $this->session->userdata('logged_in');
+	$podtvr=0;
+
+	$data_user = $this->db_module->get_user_by_id($url_id);
+	$whopage= $this->_get_whopage($url_id,$user_id);
 	if ($logged == TRUE) {
 		$podtvr = $this->db_module->get_podtvr($url_id);
 	}
@@ -35,23 +38,25 @@ class Id extends CI_Controller {
                        );
 	if (!empty($data_user)){
 	$this->load->view('userpage',$data);
-	}
-	else {
-		echo "idinahuj";
-	}
+
 	//Photo show in page user  //
 	$photo_data = $this->db_module->get_user_photos($url_id);
 	$photo_data_arr = array( 'user_data' => $photo_data );
 	$this->load->view('photos',$photo_data_arr);
-
+	} else {
+		echo "user not found";
+	}
 			}
 
  function photos() {
-	$url_id = $this->uri->segment(1);
-	$url_id = trim($url_id, " \id.");
+	if (!empty($data_user)){
+	$url_id= $this->_get_url_id();
 	$photo_data = $this->db_module->get_user_photos($url_id);
 	$photo_data_arr = array( 'user_data' => $photo_data );
 	$this->load->view('photos',$photo_data_arr);
+		} else {
+		echo "user not found";
+	}
 }
 }
 ?>
