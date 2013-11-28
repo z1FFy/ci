a85c772430e86d1967d5268dbb3cf92429ef6090
tai1z <tailz440@mail.ru>
2013-11-28 18:14:28 +0400

    удаление фотографий

--- application/modules/id/views/albom_index.php
+++ application/modules/id/views/albom_index.php
@@ -46,7 +46,9 @@
 
 			echo '<a class="photo" link="'.$this->config->site_url().'id/albom/view_photo?photo='.$item->url_photo.'&id_photos='.$item->id_photos.'&id_user='.$item->id_user.'">
 			<img style="border:4px solid #a9d5f2" src="'.$this->config->site_url().'uploads/photos/'.$item->url_photo.'" width="" height="150"></a>';
-			echo '<a class="like_photos" link='.$item->id_photos.'>LIKE</a> Понравилось: '.$item->like_photos.'<br>';
+			echo '<a class="like_photos" link='.$item->id_photos.'>LIKE</a> Понравилось: '.$item->like_photos.
+			'  <a class="delete_photos" link='.$item->id_photos.'>Удалить</a>'.
+			'<br>';
 
 		}	?>
 
